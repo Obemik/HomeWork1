1f187d60f398e62b683e2f0188ef618978a5d6d0
Obemik <misaperepecaj@gmail.com>
2024-04-21 23:22:37 +0300

    Home ex1 done

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\"Windmill, windmill for the land\n"
+        "Turn forever hand in hand\n"
+        "Take it all in on your stride\n"
+        "It is ticking, falling down\n"
+        "Love forever, love is freely\n"
+        "Turned forever, you and me\n"
+        "Windmill, windmill for the land\n"
+        "Is everybody in?\"\n"
+        "Gorillaz\n\n" << endl;
+    return 0;
+}
