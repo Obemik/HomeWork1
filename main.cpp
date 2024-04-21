885c086e4d1a203c2d7ebb42f1a7261f925f883e
Obemik <misaperepecaj@gmail.com>
2024-04-21 23:26:07 +0300

    Home ex3 done

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    cout << "Продам кота\n"
+        "Порода: Сіамська кішка\n"
+        "Рік народження: 2023\n"
+        "Вага: 10кг\n"
+        "Має паспорт\n"
+        "Колір: оранжевий\n"
+        "Ціна: 200 $\n"
+        "Телефон: 068-127-11-81\n"
+        "068      |   068   |\n"
+        "127      |   127   |\n"
+        "11       |   11    |\n"
+        "81       |   81    |" << endl;
+    return 0;
+}
